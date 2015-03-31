fe875c6e33d3565dc0ac1de845965001de433514
Ian Lamb <ianlamb32@gmail.com>
2015-03-30 22:09:54 -0400

    more changes to grunt file, not ready for build cycle yet

--- gruntfile.js
+++ gruntfile.js
@@ -14,17 +14,25 @@ module.exports = function(grunt) {
         }
       }
     },
+    copy: {
+      dist: {
+        expand: true,
+        cwd: './',
+        src: ['app/**/*'],
+        dest: 'dist/'
+      }
+    },
     concat: {
       options: {
         separator: ';'
       },
       js: {
-        src: ['public/js/**/*.js'],
-        dest: 'public/dist/js/<%= pkg.name %>.js'
+        src: ['dist/js/**/*.js'],
+        dest: 'dist/js/<%= pkg.name %>.js'
       },
       css: {
-        src: ['public/css/**/*.css'],
-        dest: 'public/dist/css/<%= pkg.name %>.css'
+        src: ['dist/css/**/*.css'],
+        dest: 'dist/css/<%= pkg.name %>.css'
       }
     },
     uglify: {
@@ -33,7 +41,14 @@ module.exports = function(grunt) {
       },
       dist: {
         files: {
-          'public/dist/js/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+          'dist/js/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+        }
+      }
+    },
+    uncss: {
+      dist: {
+        files: {
+          'dist/css/<%= pkg.name %>.css': ['dist/index.html', 'dist/views/**/*.html']
         }
       }
     },
@@ -42,8 +57,11 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
       },
       dist: {
+        options: {
+          ignore: []
+        },
         files: {
-          'public/dist/css/<%= pkg.name %>.min.css': ['<%= concat.css.dest %>']
+          'dist/css/<%= pkg.name %>.min.css': ['<%= concat.css.dest %>']
         }
       }
     },
@@ -51,27 +69,43 @@ module.exports = function(grunt) {
       dynamic: {
         files: [{
           expand: true,
-          cwd: 'public/images/',
+          cwd: 'dist/images/',
           src: ['**/*.{png,jpg,gif}'],
-          dest: 'public/dist/images/'
+          dest: 'dist/images/'
         }]
       }
     },
+    processhtml: {
+      dist: {
+        files: {
+          'dist/index.html': ['dist/index.html']
+        }
+      }
+    },
     watch: {
-      files: ['<%= concat.js.src %>', '<%= concat.css.src %>'],
-      tasks: ['concat', 'uglify', 'cssmin']
+      js: {
+        files: ['<%= concat.js.src %>'],
+        tasks: ['concat', 'uglify']
+      },
+      css: {
+        files: ['<%= concat.css.src %>'],
+        tasks: ['concat', 'cssmin']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-uncss');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-processhtml');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('test', []);
 
-  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+  grunt.registerTask('default', ['copy', 'concat', 'uglify', 'cssmin', 'imagemin', 'processhtml']);
 
 };
