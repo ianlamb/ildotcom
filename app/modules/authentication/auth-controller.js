be0af261e173e1950f868e86a7649253c74c02b5
Ian Lamb <ianlamb32@gmail.com>
2015-07-19 23:19:35 +0200

    refactor auth

---
+++ app/modules/authentication/auth-controller.js
@@ -0,0 +1,22 @@
+var auth          = require('../../middleware/auth');
+var AuthProvider  = require('./auth-provider');
+
+module.exports = function(router) {
+    'use strict';
+    
+    var authProvider = new AuthProvider();
+    
+    router.post('/auth', function(req, res) {
+        authProvider.login(req.body.password)
+            .then(function(token) {
+                res.send(token);
+            })
+            .catch(function(err) {
+                res.send(401);
+            });
+    });
+    
+    router.get('/testAuth', auth, function(req, res) {
+        res.send('You are authenticated!');
+    });
+};
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,3 @@
-var jwt     = require('jwt-simple');
-var auth    = require('./middleware/auth.js');
-var config  = require('../config/app.js');
 var Environment = require('../config/environment.js');
 var env = new Environment();
 
@@ -8,6 +5,7 @@ module.exports = function(app, router) {
     'use strict';
     
     // register all modules
+    require('./modules/authentication/auth-controller')(router);
     require('./modules/blog/blog-controller')(router);
     require('./modules/climbing/climbing-controller')(router);
     require('./modules/travel/travel-controller')(router);
@@ -23,28 +21,6 @@ module.exports = function(app, router) {
     // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
     router.get('/', function(req, res) {
         res.json({ message: 'Welcome to the ianlamb.com web API' });
-    });
-
-    // authentication
-    router.post('/auth', function(req, res) {
-        if (!req.body.password || !config.authSecret || req.body.password !== config.authSecret) {
-            return res.send(401);
-        }
-
-        var expires = new Date().getTime() + (config.tokenExpiry);
-        var token = jwt.encode({
-            iss: 'root',
-            exp: expires
-        }, app.get('jwtTokenSecret'));
-         
-        res.json({
-            token: token,
-            expires: expires
-        });
-    });
-    
-    router.get('/testAuth', auth, function(req, res) {
-        res.send('You are authenticated!');
     });
 
     // register api routes prefixed with /api
