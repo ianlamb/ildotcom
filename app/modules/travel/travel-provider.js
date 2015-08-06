6f5994a9e997cb989760af8bc69fc77207499953
Ian Lamb <ianlamb32@gmail.com>
2015-08-06 18:44:15 -0500

    modified saveTrip to update as well as create

--- app/modules/travel/travel-provider.js
+++ app/modules/travel/travel-provider.js
@@ -24,72 +24,85 @@ module.exports = function() {
     
     this.saveTrip = function(data) {
         return new Promise(function(resolve, reject) {
-            var trip = new Trip(data);
-            var promises = [];
-
-            trip.startDate = new Date(trip.startDate);
-            trip.endDate = new Date(trip.endDate);
-
-            if(data.photosetId) {
-                var promise = new Promise(function(resolve, reject) {
-                    retrievePhotoset(data.photosetId).then(function(data) {
-                        data.photoset.photo.forEach(function(photo) {
-                            var newImage = {};
-                            newImage.url = 'https://www.flickr.com/photos/' + data.photoset.owner + '/' + photo.id + '/in/set-' + data.photoset.id;
-                            newImage.thumb = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_s.jpg';
-                            newImage.title = photo.title;
-                            trip.photos.push(newImage);
-                        });
-                        console.log('added ' + trip.photos.length + ' photos to trip');
-                        resolve();
-                    }, reject);
-                });
-                promises.push(promise);
-            }
-
-            if(data.locations) {
-                data.locations.forEach(function(location) {
+            Trip.findOne({ _id: data._id }, function(err, trip) {
+                var promises = [];
+            
+                if (err) {
+                    reject(err);
+                }
+                if (!trip) {
+                    trip = new Trip(data);
+                } else {
+                    for (var prop in trip) {
+                        if (data.hasOwnProperty(prop) && data[prop]) {
+                            trip[prop] = data[prop];
+                        }
+                    }
+                }
+    
+                trip.startDate = new Date(trip.startDate);
+                trip.endDate = new Date(trip.endDate);
+    
+                if(data.photosetId) {
                     var promise = new Promise(function(resolve, reject) {
-                        Place.find({ city: location.city, country: location.country }, function(err, place) {
-                            if (err) {
-                                console.error(err);
-                                reject(err);
-                            }
-                            if (!place) {
-                                place = new Place(location);
-                                place.save(function(err, newPlace) {
-                                    if (err) {
-                                        reject(err);
-                                    }
-                                    trip.places.push(newPlace._id);
-                                    console.log('place created '  + newPlace.city + ', ' + newPlace.country);
-                                    resolve();
-                                });
-                            } else {
-                                trip.places.push(place._id);
-                                console.log('place found '  + place.city + ', ' + place.country);
-                                resolve();
-                            }
-                        });
-                        resolve();
+                        retrievePhotoset(data.photosetId).then(function(data) {
+                            data.photoset.photo.forEach(function(photo) {
+                                var newImage = {};
+                                newImage.url = 'https://www.flickr.com/photos/' + data.photoset.owner + '/' + photo.id + '/in/set-' + data.photoset.id;
+                                newImage.thumb = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_s.jpg';
+                                newImage.title = photo.title;
+                                trip.photos.push(newImage);
+                            });
+                            console.log('added ' + trip.photos.length + ' photos to trip');
+                            resolve();
+                        }, reject);
                     });
                     promises.push(promise);
+                }
+    
+                if(data.locations) {
+                    data.locations.forEach(function(location) {
+                        var promise = new Promise(function(resolve, reject) {
+                            Place.find({ city: location.city, country: location.country }, function(err, place) {
+                                if (err) {
+                                    console.error(err);
+                                    reject(err);
+                                }
+                                if (!place) {
+                                    place = new Place(location);
+                                    place.save(function(err, newPlace) {
+                                        if (err) {
+                                            reject(err);
+                                        }
+                                        trip.places.push(newPlace._id);
+                                        console.log('place created '  + newPlace.city + ', ' + newPlace.country);
+                                        resolve();
+                                    });
+                                } else {
+                                    trip.places.push(place._id);
+                                    console.log('place found '  + place.city + ', ' + place.country);
+                                    resolve();
+                                }
+                            });
+                            resolve();
+                        });
+                        promises.push(promise);
+                    });
+                }
+    
+                Promise.all(promises).then(function() {
+                    console.log('promises resolved, saving trip...');
+                    trip.save(function(err, savedTrip) {
+                        if(err) {
+                            console.error(err);
+                        }
+                        console.log('saved ' + savedTrip.name);
+                        resolve(savedTrip);
+                    });
+                }, function(err) {
+                    console.error(err);
+                    reject();
                 });
-            }
-
-            Promise.all(promises).then(function() {
-                console.log('promises resolved, saving trip...');
-                trip.created_at = new Date();
-                trip.save(function(err, savedTrip) {
-                    if(err) {
-                        console.error(err);
-                    }
-                    console.log('created ' + trip.name);
-                    resolve(savedTrip);
-                });
-            }, function(err) {
-                console.error(err);
-                reject();
             });
         });
     };
