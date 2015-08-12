fb6e4fb246eb4a3645e8a03e59f5cc0bf1a6505a
Ian Lamb <ianlamb32@gmail.com>
2015-08-12 00:23:22 -0400

    create navbar component

--- public/js/appDirectives.js
+++ public/js/appDirectives.js
@@ -1,40 +1,5 @@
 angular.module('appDirectives', [])
 
-    .directive('navbar', function() {
-        'use strict';
-
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: 'views/templates/navbar.html',
-            controller: function($scope, $location) {
-                $scope.navLinks = [
-                    { slug: 'home', text: 'Home', sortRank: 0 },
-                    { slug: 'blog', text: 'Journal', sortRank: 1 },
-                    //{ slug: 'work', text: 'Work', sortRank: 2 },
-                    { slug: 'resume', text: 'Résumé', sortRank: 3 },
-                    { slug: 'adventure', text: 'Adventure', sortRank: 4 },
-                    { slug: 'gaming', text: 'Gaming', sortRank: 5 },
-                    { slug: 'about', text: 'About', sortRank: 6 }
-                ];
-
-                $scope.isActive = function(slug) {
-                    var baseNav;
-                    var urlParts = $location.path().split('/');
-                    if (urlParts.length > 1 && urlParts[1] !== '') {
-                        baseNav = urlParts[1];
-                    } else {
-                        baseNav = 'home';
-                    }
-                    if(baseNav === slug) {
-                        return 'active';
-                    }
-                    return '';
-                };
-            }
-        };
-    })
-
     .directive('footer', function() {
         'use strict';
 
