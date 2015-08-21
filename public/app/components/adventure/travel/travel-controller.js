f0c6fce0a152daf0be15c0705c2ec5a13967e3ef
Ian Lamb <ianlamb32@gmail.com>
2015-08-21 00:33:23 -0400

    switch jvectormap for raphael and make contact-map directive into generic world-map

--- public/app/components/adventure/travel/travel-controller.js
+++ public/app/components/adventure/travel/travel-controller.js
@@ -8,63 +8,24 @@ angular.module('app.adventure.travel', [])
                 $scope.continentsVisited = 2; // TODO: un-hardcode
                 $scope.countriesVisited = 0;
                 $scope.citiesVisited = 0;
+                $scope.geoData = [];
                 var countryData = {};
-                var geoData = [];
             
                 $scope.trips.forEach(function(trip) {
                     trip.places.forEach(function(place) {
                         var geoObject = {
-                            latLng: [place.lat, place.lng],
+                            latitude: place.lat,
+                            longitude: place.lng,
                             name: place.city + ', ' + place.countryCode
                         };
-                        geoData.push(geoObject);
+                        $scope.geoData.push(geoObject);
                         if(!countryData[place.countryCode]) {
                             countryData[place.countryCode] = 10;
                             $scope.countriesVisited++;
                         }
                     });
                 });
-                $scope.citiesVisited = geoData.length;
-    
-                $('#map').vectorMap({
-                    map: 'world_mill_en',
-                    backgroundColor: 'transparent',
-                    zoomOnScroll: false,
-                    regionStyle: {
-                        initial: {
-                            fill: '#ccc',
-                            'fill-opacity': 0.7
-                        },
-                        hover: {
-                            'fill-opacity': 1
-                        }
-                    },
-                    markerStyle: {
-                        initial: {
-                            fill: '#DA45F7',
-                            'fill-opacity': 1,
-                            'stroke-fill': '#DA45F7',
-                            'stroke-width': 2,
-                            'stroke-opacity': 0.5,
-                            r: 3
-                        },
-                        hover: {
-                            'stroke-fill': '#DA45F7',
-                            'stroke-width': 6,
-                            'stroke-opacity': 0.5,
-                        }
-                    },
-                    series: {
-                        regions: [{
-                            values: countryData,
-                            scale: ['#009CF3']
-                        }]
-                    },
-                    normalizeFunction: 'polynomial',
-                    hoverOpacity: 1,
-                    hoverColor: false,
-                    markers: geoData
-                });
+                $scope.citiesVisited = $scope.geoData.length;
             });
         
         $scope.formatStartEndDates = function(startDate, endDate) {
