face10c4e183094d376307ea664014bb2819439f
Ian Lamb <ianlamb32@gmail.com>
2016-03-05 20:32:41 -0800

    fix log coloring

--- app/logger.js
+++ app/logger.js
@@ -1,22 +1,6 @@
 var winston = require('winston');
 var env = require('config/environment');
 
-winston.setLevels({
-    debug: 0,
-    info:  1,
-    silly: 2,
-    warn:  3,
-    error: 4,
-});
-
-winston.addColors({
-    debug: 'green',
-    info:  'cyan',
-    silly: 'magenta',
-    warn:  'yellow',
-    error: 'red'
-});
-
 if (env.name === 'prod') {
     winston.level = 'warn';
 } else {
@@ -27,7 +11,8 @@ var logger = new (winston.Logger)({
     transports: [
         new (winston.transports.Console)({
             level: winston.level,
-            timestamp: true
+            timestamp: true,
+            colorize: true
         })
     ]
 });
