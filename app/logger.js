f11fcfb5495f1cf5e69faf64e343fd1470f6219d
Ian Lamb <ianlamb32@gmail.com>
2016-03-05 20:48:51 -0800

    add file logging for prod

--- app/logger.js
+++ app/logger.js
@@ -1,22 +1,33 @@
 var winston = require('winston');
 var env = require('config/environment');
 
+var transports = [];
+
 if (env.name === 'prod') {
     winston.level = 'warn';
+    transports.push(
+        new (winston.transports.File)({
+            filename: 'logs/app.log',
+            level: winston.level,
+            timestamp: true
+        })
+    );
 } else {
     winston.level = 'debug';
 }
 
+transports.push(
+    new (winston.transports.Console)({
+        level: winston.level,
+        timestamp: true,
+        colorize: true
+    })
+);
+
 var logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({
-            level: winston.level,
-            timestamp: true,
-            colorize: true
-        })
-    ]
+    transports: transports
 });
 
-logger.info('chill winston, the logs are being captured on the console');
+logger.info('chill winston, the logs are being captured 2 ways - console and file');
 
 module.exports = logger;
